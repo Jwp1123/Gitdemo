5d19d79fde1555a436de4824a4094c672bb180c2
Jwp1123 <105111794+Jwp1123@users.noreply.github.com>
2022-05-08 11:05:10 +0800

    Update GitTest.java

--- src/main/java/cn/guigu/GitTest.java
+++ src/main/java/cn/guigu/GitTest.java
@@ -9,6 +9,9 @@ public class GitTest {
         System.out.println("我是m 我修改联网");
         System.out.println("我是江 我也是修改的");
         System.out.println("push");
+         System.out.println("我是远程拉过来的");
+         System.out.println("pull");
+        
 
     }
 }
